f3f9e48ed2cffe9313f99960cf9fbc8fadb0e4d9
Charles Ngo <charlesngo37@gmail.com>
2022-04-26 22:19:22 -0700

    create js to parse data to tweet html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,58 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = `
+  <article class="tweet-container">
+    <header class="header-tweet">
+      <div class="header-tweet-profile">
+        <img class ="profile-picture" src=${tweet.user.avatars}/>
+        <p>${tweet.user.name}</p>
+      </div>
+      <p class ="header-tweet-handle">${$tweet.user.handle}</p>
+    </header>
+    <p class="tweet-body"">${tweet.content.text}</p>
+    <footer class="footer-tweet">
+      <p>${tweet.created_at}</p>
+      <div class="social-media-buttons">
+        <p><i class="fa-solid fa-flag"></i></p>
+        <p><i class="fa-solid fa-retweet"></i></p>
+        <p><i class="fa-solid fa-heart"></i></p>
+      </div>
+    </footer>
+  </article>`;
+  return $tweet;
+}
+
+renderTweets(data);
