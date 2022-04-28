72ea4143124e070e761f061a93c426e6d16c44b2
Charles Ngo <charlesngo37@gmail.com>
2022-04-27 23:03:23 -0700

    updated color palette

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".right-nav").click(function() {
-    const $tweetForm = $(this).parent().siblings().last().children().first();
+    const $tweetForm = $(this).parent().next().next().children().first();
     $tweetForm.slideToggle("slow");
+    $(window).scrollTop(0);
   });
 });
