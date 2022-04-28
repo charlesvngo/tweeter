88f5525847b4a297ef578fa1baa3dbac83423200
Charles Ngo <charlesngo37@gmail.com>
2022-04-27 18:12:49 -0700

    removed unnecessary scripts

--- public/scripts/box-shadow.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".tweet-container").hover(function() {
-    $(this).addClass("shadow");
-  }, function() {
-    $(this).removeClass("shadow");
-  });
-});
--- public/scripts/button-hover.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".social-buttons").hover(function() {
-    $(this).addClass("highlight");
-  }, function() {
-    $(this).removeClass("highlight");
-  });
-});
