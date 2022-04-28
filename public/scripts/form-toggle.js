609899e1614bef66b8efe7b995e3d22afdd72db1
Charles Ngo <charlesngo37@gmail.com>
2022-04-27 21:10:27 -0700

    complete toggle button for tweet form

---
+++ public/scripts/form-toggle.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $(".right-nav").click(function() {
+    const $tweetForm = $(this).parent().siblings().last().children().first();
+    $tweetForm.slideToggle("slow");
+  });
+
+});
