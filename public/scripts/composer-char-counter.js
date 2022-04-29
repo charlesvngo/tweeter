5efb8286ab3bafe5ac2ee1fc1ca942f2a534e68f
Charles Ngo <charlesngo37@gmail.com>
2022-04-29 16:13:13 -0700

    removed console.logs from char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#tweet-text").on("input", function(event) {
+  $("#tweet-text").on("input", function() {
     let $counter = $(this).closest("form").find(".counter");
     let tweetLength = $(this).val().length;
     $counter.val(140 - tweetLength);
@@ -8,14 +8,12 @@ $(document).ready(function() {
     } else {
       $counter.removeClass("negative");
     }
-    console.log(event);
   });
 
   // If the enter key is pressed, send the tweet out.
   $("#tweet-text").on("keyup", function(event) {
     if (event.originalEvent.key === 'Enter') {
       let $submitButton = $(this).closest("form").find(".tweet-button");
-      console.log($submitButton);
       $submitButton.submit();
     }
   });
