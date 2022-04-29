63e2d887adac846e135e2ac5dbef4034a3148944
Charles Ngo <charlesngo37@gmail.com>
2022-04-28 20:57:17 -0700

    update form toggle dom traversal method

--- public/scripts/form-toggle.js
+++ public/scripts/form-toggle.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".right-nav").click(function() {
-    const $tweetForm = $(this).parent().next().next().children(":first-child");
+    const $tweetForm = $(this).parent().siblings("main").children(":first-child");
     $tweetForm.slideToggle("slow");
     $("html").animate({ scrollTop: 0 }, 600);
     $tweetForm.children(":last-child").children(":nth-child(2)").focus();
