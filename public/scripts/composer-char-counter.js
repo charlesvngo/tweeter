d63ea650c10c29c6de4da6b05e612df75959759f
Charles Ngo <charlesngo37@gmail.com>
2022-04-29 15:40:19 -0700

    updated char counter script

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
   $("#tweet-text").on("input", function(event) {
-    let counter = $(this).next().children(":last-child");
+    let $counter = $(this).closest("form").find(".counter");
     let tweetLength = $(this).val().length;
-    counter.val(140 - tweetLength);
-    if (counter.val() < 0) {
-      counter.addClass("negative");
+    $counter.val(140 - tweetLength);
+    if ($counter.val() < 0) {
+      $counter.addClass("negative");
     } else {
-      counter.removeClass("negative");
+      $counter.removeClass("negative");
     }
-    // If the enter key is pressed, send the tweet out.
+    console.log(event);
+  });
+
+  // If the enter key is pressed, send the tweet out.
+  $("#tweet-text").on("keyup", function(event) {
     if (event.originalEvent.key === 'Enter') {
-      let $submitButton = $(this).next().children(":first-child");
+      let $submitButton = $(this).closest("form").find(".tweet-button");
+      console.log($submitButton);
       $submitButton.submit();
     }
   });
